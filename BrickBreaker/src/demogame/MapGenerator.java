1b10885687ccc5771c5d4330d0e6a10bedaeee2a
KunalSharma0406 <118346621+KunalSharma0406@users.noreply.github.com>
2022-11-17 17:05:43 +0530

    Add files via upload

---
+++ BrickBreaker/src/demogame/MapGenerator.java
@@ -0,0 +1,36 @@
+package demogame;
+import java.awt.*;
+
+public class MapGenerator {
+    public int map[][];
+    public int bricksWidth;
+    public int brickHeight;
+    public MapGenerator(int row,int col){
+        map=new int[row][col];
+        for(int[] map1:map){
+    for(int j=0;j<map[0].length;j++){
+        map1[j]=1;
+    }
+        }
+        bricksWidth=540/col;
+        brickHeight=150/row;
+    }
+    public void draw(Graphics2D g){
+        for(int i=0;i< map.length;i++){
+            for(int j=0;j<map[0].length;j++){
+                if(map[i][j]>0){
+                    g.setColor(Color.red);
+                    g.fillRect(j*bricksWidth+80,i*brickHeight+50,bricksWidth,brickHeight);
+                    g.setStroke(new BasicStroke(3));
+                    g.setColor(Color.black);
+                    g.drawRect(j*bricksWidth+80,i*brickHeight+50,bricksWidth,brickHeight);
+                }
+            }
+        }
+        }
+
+    public void SetBrickValue(int value, int row, int col) {
+        map[row][col] = value;
+    }
+}
+
